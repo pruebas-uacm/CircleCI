b56f662fa10516475d48104c017b9aea1a1b2ee5
Profesor <cerjio@gmail.com>
2019-06-06 00:39:52 -0500

    cambiando test

--- src/test/java/mx/edu/uacm/circleci/service/impl/UsuarioServiceImplTest.java
+++ src/test/java/mx/edu/uacm/circleci/service/impl/UsuarioServiceImplTest.java
@@ -5,12 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import lombok.extern.slf4j.Slf4j;
+
 import mx.edu.uacm.circleci.domain.Usuario;
 import mx.edu.uacm.circleci.service.UsuarioService;
 
 
-@Slf4j
 @SpringBootTest
 public class UsuarioServiceImplTest {
 	
@@ -19,7 +18,7 @@ public class UsuarioServiceImplTest {
 	
 	@Test
 	public void debeAgregarUnUsuario() throws Exception {
-		log.debug("Entrando a debeAgregarUnUsuario");
+	
 		
 		Usuario usuario = new Usuario();
 		usuario.setNombre("Prueba");
